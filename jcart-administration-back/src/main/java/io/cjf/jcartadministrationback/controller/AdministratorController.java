4b172b08ede09aa9634cf85e13b5cb81c87ae307
崔 <cui520.cn@126.com>
2020-02-25 14:00:15 +0800

    draft done administrator api

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -1,8 +1,10 @@
 package io.cjf.jcartadministrationback.controller;
 
-import io.cjf.jcartadministrationback.dto.in.AdministratorLoginInDTO;
-import io.cjf.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.cjf.jcartadministrationback.dto.in.*;
 import io.cjf.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorShowOutDTO;
+import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +17,7 @@ public class AdministratorController {
     }
 
     @GetMapping("/getProfile")
-    public AdministratorGetProfileOutDTO getProfile(Integer adminstratorId){
+    public AdministratorGetProfileOutDTO getProfile(@RequestParam(required = false) Integer adminstratorId){
         return null;
     }
 
@@ -24,4 +26,33 @@ public class AdministratorController {
 
     }
 
+    @GetMapping("/getPadResetCode")
+    public String getPadResetCode(@RequestParam String email){
+        return  null;
+    }
+
+    @PostMapping("/resetPwd")
+    public void restPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
+
+    }
+
+    @GetMapping("/getList")
+    public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
+        return null;
+    }
+
+    @GetMapping("/getById")
+    private AdministratorShowOutDTO getById(@RequestParam Integer administratorId){
+        return  null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody AdministratorCreateInDTO administratorCreateInDTO){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
+
+    }
 }
---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/dto/in/AdministratorUpdateInDTO.java
@@ -0,0 +1,54 @@
+package io.cjf.jcartadministrationback.dto.in;
+
+/**
+ * @Author CuiJiaCheng
+ * @Description
+ * @create 2020-02-25 13:57
+ **/
+public class AdministratorUpdateInDTO {
+    private Integer administratorId;
+    private String realName;
+    private String password;
+    private  String email;
+    private Byte status;
+
+    public Integer getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(Integer administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
