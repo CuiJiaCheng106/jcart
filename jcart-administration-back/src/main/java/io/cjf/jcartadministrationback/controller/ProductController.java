64cd057cc51b094a02fba8f5314d7459477c9bec
崔 <cui520.cn@126.com>
2020-02-26 15:19:54 +0800

    add administrator product create

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ProductController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ProductController.java
@@ -6,6 +6,8 @@ import io.cjf.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
 import io.cjf.jcartadministrationback.dto.out.ProductListOutDTO;
 import io.cjf.jcartadministrationback.dto.out.ProductShowOutDTO;
+import io.cjf.jcartadministrationback.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/search")
     public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,@RequestParam Integer pageNum){
@@ -30,7 +35,8 @@ public class ProductController {
 
     @PostMapping("/create")
     public Integer create(@RequestBody ProductCreateInDTO productCreateInDTO){
-        return null;
+        Integer productId = productService.create(productCreateInDTO);
+        return productId;
     }
 
     @PostMapping("/update")
---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/ProductService.java
@@ -0,0 +1,14 @@
+package io.cjf.jcartadministrationback.service;
+
+import io.cjf.jcartadministrationback.dto.in.ProductCreateInDTO;
+import io.cjf.jcartadministrationback.po.Product;
+
+/**
+ * @Author CuiJiaCheng
+ * @Description
+ * @create 2020-02-26 14:34
+ **/
+public interface ProductService {
+    Integer create(ProductCreateInDTO productCreateInDTO);
+
+}
