2f0ce6b4542a9c23c807600d8f9cc99ec5ad0829
崔 <cui520.cn@126.com>
2020-02-26 13:29:23 +0800

    add product apis

---
+++ jcart-store-back/src/main/java/io/cjf/jcartstoreback/controller/ProductController.java
@@ -0,0 +1,24 @@
+package io.cjf.jcartstoreback.controller;
+
+import dto.in.ProductSearchInDTO;
+import dto.out.PageOutDTO;
+import dto.out.ProductListOutDTO;
+import dto.out.ProductShowOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/search")
+    public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
+                                                @RequestParam Integer pageNum){
+        return null;
+    }
+
+    @GetMapping("/getById")
+    public ProductShowOutDTO getById(@RequestParam Integer productId){
+        return null;
+    }
+
+}
