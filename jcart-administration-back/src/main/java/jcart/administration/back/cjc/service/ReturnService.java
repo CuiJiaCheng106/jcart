2e054b34c6797048d9542386997c81ba3a05766b
崔 <cui520.cn@126.com>
2020-03-10 17:29:41 +0800

    add return apis

---
+++ jcart-administration-back/src/main/java/jcart/administration/back/cjc/service/ReturnService.java
@@ -0,0 +1,15 @@
+package io.cjf.jcartadministrationback.service;
+
+import com.github.pagehelper.Page;
+import io.cjf.jcartadministrationback.dto.in.ReturnSearchInDTO;
+import io.cjf.jcartadministrationback.po.Return;
+
+public interface ReturnService {
+
+    Page<Return> search(ReturnSearchInDTO returnSearchInDTO, Integer pageNum);
+
+    Return getById(Integer returnId);
+
+    void update(Return aReturn);
+
+}
