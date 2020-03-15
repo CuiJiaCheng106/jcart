22a2d432d1b8582a0274e8e763b650842572649c
崔 <cui520.cn@126.com>
2020-03-15 17:40:28 +0800

    fix page

--- jcart-administration-front/page/router.js
+++ jcart-administration-front/page/router.js
@@ -1,8 +1,10 @@
 const routes = [
+    { path: '/administrator/updateprofile', component: AdministratorUpdateProfileRoutePage },
     { path: '/product/search', component: ProductSearchRoutePage },
     { path: '/customer/search', component: CustomerSearchRoutePage },
     { path: '/order/search', component: OrderSearchRoutePage },
     { path: '/return/search', component: ReturnSearchRoutePage },
+    { path: '/customer/show/:customerId', component: CustomerShowRoutePage },
     { path: '/administrator/index', component: AdministratorindexRoutePage },
     { path: '/product/update:/productId', component: ProductUpdateRoutePage }
 ];
